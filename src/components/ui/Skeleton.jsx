9514946ec741db69e03ffc99e5dc194e12c32a06
941-life <truzww@gmail.com>
2025-07-10 10:39:42 +0900

    [feat] skeleton ui component
    - 공통 기본 skeleton 설계
    - animation 추가

---
+++ src/components/ui/Skeleton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./skeleton.css";
+const SkeletonElement = ({ type }) => {
+    const classes = `skeleton ${type}`;
+    return <div className={classes}></div>;
+};
+
+const Shimmer = () => {
+    return (
+        <div className="shimmer-wrapper">
+            <div className="shimmer"></div>
+        </div>
+    );
+};
+
+export { Shimmer };
+export default SkeletonElement;
