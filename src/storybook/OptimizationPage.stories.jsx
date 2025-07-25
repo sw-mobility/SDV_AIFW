90f982cc45090ab06aec8178964d96f74063e89e
941-life <truzww@gmail.com>
2025-07-25 16:57:44 +0900

    [storybook]

---
+++ src/storybook/OptimizationPage.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import OptimizationPage from '../pages/optimization_page/OptimizationPage.jsx';
+
+export default {
+  title: 'Pages/OptimizationPage',
+  component: OptimizationPage,
+};
+
+export const Default = () => <OptimizationPage />; 
